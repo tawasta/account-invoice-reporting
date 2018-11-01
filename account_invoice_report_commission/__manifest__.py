30f5dc7c96b14022b86984e0924a859de33bd32f
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-11-01 12:07:45 +0200

    Update i18n

--- account_invoice_report_commission/__manifest__.py
+++ account_invoice_report_commission/__manifest__.py
@@ -23,7 +23,7 @@
     'name': 'Invoice Analysis: Commission Paid Field',
     'summary': "Enable grouping by invoice's commission field",
     'category': 'Invoicing & Payments',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
     'license': 'AGPL-3',
--- account_invoice_report_invoice_address/__manifest__.py
+++ account_invoice_report_invoice_address/__manifest__.py
@@ -23,7 +23,7 @@
     'name': 'Invoice Analysis: Invoice Address Field',
     'summary': "Enable grouping by invoice address",
     'category': 'Invoicing & Payments',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
     'license': 'AGPL-3',
