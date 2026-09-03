3273ee5f2f2b650d46431666d798b24aab145332
Jarmo Kortetjärvi <jarmo.kortetjarvi@futural.fi>
2026-09-03 13:47:39 +0300

    [IMP] Show bank account owner name instead of company name

--- account_report_invoice_bank_transfer/__manifest__.py
+++ account_report_invoice_bank_transfer/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Account Report Invoice Bank Transfer",
     "summary": "Add a bank transfer section for invoices",
-    "version": "19.0.1.0.0",
+    "version": "19.0.1.0.1",
     "category": "Reporting",
     "website": "https://github.com/tawasta/account-invoice-reporting",
     "author": "Futural",
